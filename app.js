4ac3058a745d0b28639cc86216c91699c091d752
Senthilvasudevan <vbsukl2000@gmail.com>
2021-05-01 13:11:48 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+var mysql = require('mysql');
+const app = express();
+
+var connection = mysql.createConnection({
+
+    host: 'localhost',
+    user:'root',
+    password:'',
+    database: 'sarath'
+})
+
+connection.connect(function(error)
+{
+    if(!!error)
+    {
+        console.log(error);
+    }
+    else{
+        console.log("connected");
+    }
+})
+app.set("view engine", "ejs");
+
+app.get('/',(req,res)=>{
+    res.render("index");
+});
+
+app.post('/uploads',(req,res)=>{
+    
+        console.log(req.body.results);
+})
+
+app.listen(5000,()=>{
+    console.log("server connected to 5000");
+  });
